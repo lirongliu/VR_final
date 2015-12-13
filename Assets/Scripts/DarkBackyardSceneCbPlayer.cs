51f6cc7f5a7e930f605f5e2474a19d72ddb370a8
Lirong Liu <ll2867@nyu.edu>
2015-12-13 13:47:08 -0500

    added GameController and player scripts; changed some file names

---
+++ Assets/Scripts/DarkBackyardSceneCbPlayer.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DarkBackyardSceneCbPlayer : NetworkedPlayer {
+
+	private GameObject hitEnemy;
+	
+
+	void Start ()
+	{
+		DontDestroyOnLoad (this);
+
+		avatar.transform.localPosition = new Vector3 (-5, 0.5f, -2);
+		
+		if (photonView.isMine) {
+			GameObject cb = GameObject.Find ("CardboardMain");
+			
+			Transform avatarHeadTransform = this.transform.Find("AvatarHead");
+			Transform avatarBodyTransform = this.transform.Find("AvatarBody");
+			
+			
+			/* hierachy:
+			 * avatar
+			 * avatarBody
+			 * CardboardMain
+			 * head
+			 */
+			avatarBodyTransform.SetParent(avatar.transform);
+			avatarBodyTransform.localPosition = Vector3.zero;
+			
+			cb.transform.SetParent(avatarBodyTransform);
+			avatarHeadTransform.SetParent(cb.transform);
+			cb.transform.localPosition = new Vector3(0, Constants.cbAvatarHeight, 0);
+			avatarHeadTransform.localPosition = Vector3.zero;
+			
+			// set head transform
+			this.headTransform = cb.transform;
+		}   else {
+
+			Transform avatarHeadTransform = this.transform.Find("AvatarHead");
+			Transform avatarBodyTransform = this.transform.Find("AvatarBody");
+			
+			/* hierachy:
+			 * avatar
+			 * avatarBody
+			 * head
+			 */
+			avatarBodyTransform.SetParent(avatar.transform);
+			avatarBodyTransform.localPosition = Vector3.zero;
+			
+			avatarHeadTransform.SetParent(avatarBodyTransform);
+			avatarHeadTransform.localPosition = new Vector3(0, Constants.cbAvatarHeight, 0);
+			
+			// set head transform
+			this.headTransform = Utility.FindTransform (avatar.transform, "AvatarHead");
+		}
+		
+		playerLocal = headTransform;
+		playerGlobal = avatar.transform;		
+	}
+	
+	void Update(){
+		
+		if (!photonView.isMine) {
+			//Update remote player (smooth this, this looks good, at the cost of some accuracy)
+			avatar.transform.localPosition = Vector3.Lerp (avatar.transform.localPosition, correctAvatarPos, Time.deltaTime * 5);
+			avatar.transform.localRotation = Quaternion.Lerp (avatar.transform.localRotation, correctAvatarRot, Time.deltaTime * 5);
+			headTransform.localRotation = Quaternion.Lerp (headTransform.localRotation, correctHeadRot, Time.deltaTime * 5);
+		} else {
+			inputHandler ();
+			RaycastHit hit;
+			Physics.Raycast (Camera.main.transform.position, Camera.main.transform.forward, out hit);
+			this.checkHitEnemies (hit, ref hitEnemy);
+			this.checkHitTbPlayer (hit);
+			
+			// only cb is responsible for generating enemies
+			if (Time.frameCount % 100 == 0) {
+				photonView.RPC ("generateEnemy", PhotonTargets.All, Random.Range (-30, 30), Random.Range (-30, 30), 100f, "chaseBoth");
+			}
+		}
+	}
+
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+	public string currScene = Constants.backyardSceneName;
+
+	// Use this for initialization
+	void Start () {
+		DontDestroyOnLoad (this);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public string getNextScene() {
+		print ("currScene " + currScene);
+		string nextScene = "";
+		if (currScene == Constants.backyardSceneName) {
+			nextScene = Constants.bossSceneName;
+		} else if (currScene == Constants.bossSceneName) {
+			nextScene = Constants.darkBackyardSceneName;
+		} else if (currScene == Constants.darkBackyardSceneName) {
+		}
+		return nextScene;
+	}
+
+	public void setCurrScene(string currScene) {
+		this.currScene = currScene;
+	}
+}
